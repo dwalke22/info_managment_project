20903659991a521fccf7bfccf9d97ca905af275f
csuser@cs.westga.edu <csuser@cs.westga.edu>
2021-10-14 12:04:15 -0400

    Implemented Login Functionality

---
+++ CS3230RentalSystemProject/CS3230RentalSystemProject/DAL/LogInDALI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace DBAccess.DAL
+{
+    /// <summary>
+    /// The LogInDALI class
+    /// </summary>
+    public class LogInDALI
+    {
+        /// <summary>
+        /// Validates the login credentials
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <param name="password">The password.</param>
+        /// <returns>
+        ///     Returns true if the 
+        /// </returns>
+        public static bool LogInValidation(string username, string password)
+        {
+            using (MySqlConnection connection = new MySqlConnection(Connection.connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query =
+                        "select username, password from login where username = @username and password = @password";
+
+                    using MySqlCommand command = new MySqlCommand(query, connection);
+                    command.Parameters.Add("@username", MySqlDbType.VarChar);
+                    command.Parameters.Add("@password", MySqlDbType.VarChar);
+
+                    using MySqlDataReader reader = command.ExecuteReader();
+
+                    bool loginSuccess = reader.Read();
+                    connection.Close();
+                    return loginSuccess;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Something went wrong");
+                    return false;
+                }
+            }
+        }
+    }
+}
