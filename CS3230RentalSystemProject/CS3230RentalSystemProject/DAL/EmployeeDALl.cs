f4e764aafd9b053494105bea9f07d58898166a92
dwalke22 <dwalke22@my.westga.edu>
2021-11-16 16:45:38 -0500

    Added ability to add employee with hashed password

--- CS3230RentalSystemProject/CS3230RentalSystemProject/DAL/EmployeeDALl.cs
+++ CS3230RentalSystemProject/CS3230RentalSystemProject/DAL/EmployeeDALl.cs
@@ -2,6 +2,7 @@
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
 
 
 namespace DBAccess.DAL
@@ -60,5 +61,71 @@ namespace DBAccess.DAL
             }
         }
 
+        /// <summary>
+        /// Adds the employee.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
+        /// <param name="isAdmin">The is admin.</param>
+        public void AddEmployee(Employee employee, int isAdmin)
+        {
+            using (MySqlConnection connection = new MySqlConnection(Connection.connectionString))
+            {
+                string query =
+                    "insert into employee (firstName, lastName, phoneNumber, email, isAdmin) values(@firstname, @lastname, @phoneNumber, @email, @isAdmin";
+
+                connection.Open();
+
+                using MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.Add("@firstname", MySqlDbType.VarChar).Value = employee.FirstName;
+                command.Parameters.Add("@lastname", MySqlDbType.VarChar).Value = employee.LastName;
+                command.Parameters.Add("@email", MySqlDbType.VarChar).Value = employee.Email;
+                command.Parameters.Add("@phoneNumber", MySqlDbType.VarChar).Value = employee.PhoneNumber;
+                command.Parameters.Add("@isAdmin", MySqlDbType.Int32).Value = isAdmin;
+
+                command.ExecuteNonQuery();
+
+            }
+        }
+
+        /// <summary>
+        /// Adds the employee username password.
+        /// </summary>
+        /// <param name="employeeid">The employeeid.</param>
+        /// <param name="username">The username.</param>
+        /// <param name="password">The password.</param>
+        public void AddEmployeeUsernamePassword(int employeeid, string username, string password)
+        {
+            using (MySqlConnection connection = new MySqlConnection(Connection.connectionString))
+            {
+                string query = "insert into login(id, username, password) values (@employeeid, @username, @password)";
+
+                connection.Open();
+                using MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.Add("@employeeid", MySqlDbType.Int32).Value = employeeid;
+                command.Parameters.Add("@username", MySqlDbType.VarChar).Value = username;
+                command.Parameters.Add("@password", MySqlDbType.VarChar).Value = password;
+
+                command.ExecuteNonQuery();
+            }
+
+        }
+
+        /// <summary>
+        /// Gets the newest employee identifier.
+        /// </summary>
+        /// <returns></returns>
+        public int GetNewestEmployeeID()
+        {
+            int id = -1;
+            using (MySqlConnection connection = new MySqlConnection(Connection.connectionString))
+            {
+                string query = "select max(employeeID) from employee";
+
+                using MySqlCommand command = new MySqlCommand(query, connection);
+                id = (int)command.ExecuteScalar();
+
+                return id;
+            }
+        }
     }
 }
---
+++ CS3230RentalSystemProject/CS3230RentalSystemProject/View/AddEmployeeContentDialog.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using CS3230RentalSystemProject.Model;
+using CS3230RentalSystemProject.Utils;
+using DBAccess.DAL;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CS3230RentalSystemProject.View
+{
+    public sealed partial class AddEmployeeContentDialog : ContentDialog
+    {
+        private EmployeeDAL employeeDal;
+
+        public AddEmployeeContentDialog()
+        {
+            this.InitializeComponent();
+            this.employeeDal = new EmployeeDAL();
+        }
+
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            Employee employee = new Employee
+            {
+                FirstName = this.employeeFirstNameBox.Text,
+                LastName = this.employeeLastNameBox.Text,
+                PhoneNumber = this.phoneNumberBox.Text,
+                Email = this.emailBox.Text
+            };
+
+            int isAdmin = 0;
+            var isChecked = this.isAdminCheckBox.IsChecked;
+            if (isChecked != null && (bool)isChecked)
+            {
+                isAdmin = 1;
+            }
+
+            this.employeeDal.AddEmployee(employee, isAdmin);
+
+            var hashedPassword = PasswordHash.CreateMD5(this.passwordBox.Text);
+            var employeeID = this.employeeDal.GetNewestEmployeeID();
+
+            this.employeeDal.AddEmployeeUsernamePassword(employeeID, this.usernameBox.Text, hashedPassword);
+        }
+
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+    }
+}
